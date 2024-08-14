7af8ae69dc6d6ef4dec1478039b13c543c77d803
Aman Varshney <av.amanvarshney11@gmail.com>
2024-08-14 16:17:22 +0530

    change name in manifest

--- figma.config.ts
+++ figma.config.ts
@@ -2,8 +2,8 @@ import type { FigmaConfig, PluginManifest } from "vite-figma-plugin/lib/types";
 import { version } from "./package.json";
 
 export const manifest: PluginManifest = {
-  name: "shadcn-svg-components", 
-  id: "amanvarshney01.figma.plugin", 
+  name: "kofferX_shadcn", 
+  id: "1405190660804971485", 
   api: "1.0.0",
   main: "code.js",
   ui: "index.html",
